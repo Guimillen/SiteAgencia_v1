ff49f54986d48eda22eace66e9cfccf4cf58bc32
Guilherme Millen Azevedo <108336927+Guimillen@users.noreply.github.com>
2022-09-05 12:11:41 -0300

    Add files via upload

---
+++ BrModelo/logico_agviagens.sql
@@ -0,0 +1,37 @@
+/* Lógico_1: */
+
+CREATE TABLE Cliente (
+    nome VARCHAR,
+    endereco VARCHAR,
+    cpf INTEGER PRIMARY KEY,
+    telefone INTEGER,
+    email VARCHAR,
+    RG VARCHAR
+);
+
+CREATE TABLE Passagem (
+    empresaArea VARCHAR,
+    numeroBilhete INTEGER PRIMARY KEY,
+    origem VARCHAR,
+    destino VARCHAR,
+    dataViagem DATE,
+    horario TIME,
+    acento INTEGER,
+    dadosPassageiro VARCHAR
+);
+
+CREATE TABLE Compra (
+    fk_Cliente_cpf INTEGER,
+    fk_Passagem_numeroBilhete INTEGER,
+    idPedido INTEGER PRIMARY KEY
+);
+ 
+ALTER TABLE Compra ADD CONSTRAINT FK_Compra_2
+    FOREIGN KEY (fk_Cliente_cpf)
+    REFERENCES Cliente (cpf)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Compra ADD CONSTRAINT FK_Compra_3
+    FOREIGN KEY (fk_Passagem_numeroBilhete)
+    REFERENCES Passagem (numeroBilhete)
+    ON DELETE RESTRICT;
